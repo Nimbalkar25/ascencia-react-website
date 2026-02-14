900d4bf6c03a8a8d69aa5a8afde3239c0395e0a4
Pratik Nimbalkar <nimbalkarpratik25@gmail.com>
2026-02-14 13:03:41 +0530

    Corrected app.jsx for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <NavBar/>
 
       <Routes>
-        <Route path="*" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
         <Route  path="/" element={<HomePage/>}/>
         <Route path="/about" element={<AboutPage/>} />
         <Route path="/contact" element={<ContactPage/>} />
